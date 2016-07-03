d6126664ff7b46818abcff6dbc043963aaee44b1
leandrosolagna@gmail.com <leandrosolagna@gmail.com>
2016-07-03 14:00:42 -0300

    script para comitar as coisas

---
+++ README.txt
@@ -0,0 +1,6 @@
+Hello there!
+
+Welcome!
+On this repository only files I've been messing with.
+It might be for school, work or even entertainment.
+You're free to copy them and do whatever you want with.
---
+++ scriptgit.sh
@@ -0,0 +1,29 @@
+##############################################################         
+#This is a simple script created to commit your files.       #
+#It needs to be inside your git folder for this to happen.   #
+#The script will get the message you write, add your files,  #
+#commit them with the message you wrote, then it will push   #
+# in your branch, switch to master, pull the changes from one# 
+#branch to master and push them to master and finally        # 
+# it will go back your branch                                #
+#                                                            #
+#Author: Leandro Solagna                                     #
+#Date: July 3rd 2016                                         #
+#                                                            #
+#Anyone can use this, just change the branches name!         #
+##############################################################
+
+MESSAGE=""
+
+echo "Enter your message for commit: "
+read MESSAGE
+
+echo "You typed: $MESSAGE"
+
+git add *
+git commit -m "$MESSAGE"
+git push origin teste
+git checkout master
+git pull origin teste
+git push origin master
+git checkout teste
