c3c02ff4b0bab7f5bd2a80b7a9aa1bcb3c681e57
Leandro Solagna <leandrosolagna@gmail.com>
2019-08-23 00:36:55 +0200

    Colors added

--- functions.sh
+++ functions.sh
@@ -23,7 +23,7 @@ function checkFile () {
 	if [[ -f docker-stack.yml ]]; then
 		return 0
 	else
-		printf "\033[0;31mPlease, run this script in the same directory as the file 'docker-stack.yml'.\e[0m\n"
+		printf "\033[0;31m\e[1mPlease, run this script in the same directory as the file 'docker-stack.yml'.\e[0m\n"
 		exit 1
 	fi
 }
@@ -31,8 +31,8 @@ function checkFile () {
 function checkSwarm () {
 
 	if [[ "$(docker info --format '{{.Swarm.LocalNodeState}}')" == "inactive" ]]; then
-		printf "Host is not in swarm mode.\n"
-		printf "Putting the host in swarm mode.\n"
+		printf "\e[34m\e[1mHost is not in swarm mode.\e[0m\n"
+		printf "\e[34m\e[1mPutting the host in swarm mode.\e[0m\n"
 		docker swarm init --advertise-addr $(ip a | grep 192.168.56 | awk '{print $(NF)}')
 	else
 		return 0
@@ -44,9 +44,9 @@ function login () {
 	if [[ "$(cat ~/.docker/config.json | grep ${PRIVATEREGISTRY})" ]]; then
 		return 0
 	else
-		printf "You need to login on the registry ${PRIVATEREGISTRY}.\n"
-		printf "docker login ${PRIVATEREGISTRY}\n"
-		printf "Please, type the your username and password. They are the same as bk-inside"
+		printf "\e[34m\e[1mYou need to login on the registry ${PRIVATEREGISTRY}.\e[0m\n"
+		printf "\e[34m\e[1mdocker login ${PRIVATEREGISTRY}\e[0m\n"
+		printf "\e[34m\e[1mPlease, type the your username and password. They are the same as your email.\e[0m\n"
 		docker login ${PRIVATEREGISTRY}
 	fi
 	
@@ -54,19 +54,19 @@ function login () {
 
 function removeAll () {
 
-	printf "This will remove all stack components (stack, network, volume)\n"
-	printf "Are you sure you want to proceed? (y/N)\n"
+	printf "\e[33m\e[1mThis will remove all stack components (stack, network, volume).\e[0m\n"
+	printf "\e[33m\e[1mAre you sure you want to proceed? (y/N)\e[0m\n"
 	read -r ANSWER
 
 	if [[ ${ANSWER} == "y" || ${ANSWER} == "Y" ]]; then
-		printf "Stopping the stack\n"
+		printf "\e[34m\e[1mStopping the stack\e[0m\n"
         	docker stack rm dev
-		printf "Removing the network\n"
+		printf "\e[34m\e[1mRemoving the network\e[0m\n"
        		docker network rm ingress
-		printf "Removing the volume\n"
+		printf "\e[34m\e[1mRemoving the volume\e[0m\n"
         	docker volume rm db-dev
 	else
-		printf "Procedure cancelled.\n"
+		printf "\e[34m\e[1mProcedure cancelled.\e[0m\n"
 	fi
 
 }
@@ -76,7 +76,7 @@ function network () {
 	if [[ "$(docker network inspect ingress)" ]]; then
 		printf "Network already exists\n"
 	else
-		printf "Creating the network 'ingress'\n"
+		printf "\e[34m\e[1mCreating the network 'ingress'.\n"
  	        docker network create --ingress --driver overlay ingress
 	fi
 
@@ -87,7 +87,7 @@ function volume () {
 	if docker volume ls | grep -q 'db-dev'; then
 		printf "Volume already exists\n"
 	else
-		printf "Creating the volume db-dev\n"
+		printf "\e[34m\e[1mCreating the volume db-dev!\e[0m\n"
          	docker volume create db-dev
 	fi
 
@@ -95,7 +95,7 @@ function volume () {
 
 function reloadStack () {
 
-	printf "Reloading the stack\n"
+	printf "\e[34m\e[1mReloading the stack!\e[0m\n"
 	docker stack deploy --with-registry-auth -c docker-stack.yml dev
 
 }
@@ -103,7 +103,7 @@ function reloadStack () {
 function startStack () {
 
 	if grep -q ${PRIVATEREGISTRY} ~/.docker/config.json; then
-		printf "Starting the stack dev\n"
+		printf "\e[34m\e[1mStarting the stack dev!\e[0m\n"
 		docker stack deploy -c docker-stack.yml dev
 	else
 		login
@@ -113,7 +113,7 @@ function startStack () {
 
 function stopStack () {
 
-	printf "Stopping the stack dev\n"
+	printf "\e[34m\e[1mStopping the stack dev.\e[0m\n"
 	docker stack rm dev
 
 }
